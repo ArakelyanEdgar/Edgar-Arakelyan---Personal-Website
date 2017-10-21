34ddc86b1baa2b969d81e5565de50b317843d9ad
lambo4bkfast <edgararakelyan123@gmail.com>
2017-10-20 21:48:28 -0700

    a

--- backend.js
+++ backend.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 var path = require("path");
 var app = express();
 
@@ -37,3 +37,50 @@ app.listen(app.get("port", function(){
     console.log("Node app is running on port", app.get("port"));
 }))
 
+*/
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+
+app.use(express.static(path.join(__dirname+"/views")));
+app.use(express.static(path.join(__dirname+"/styles")))
+app.use(express.static(path.join(__dirname+"/javascript")))
+
+//page routers
+app.route("/")
+    .get(function(req,res, next){
+        res.sendFile(path.join(__dirname + '/views/main.html'));
+    })
+app.route("/about.html")
+    .get(function(req,res, next){
+        res.sendFile(path.join(__dirname + '/views/about.html'));
+})
+app.route("/application.html")
+    .get(function(req,res, next){
+        res.sendFile(path.join(__dirname + '/views/application.html'));
+})
+app.route("/contact_us.html")
+    .get(function(req,res, next){
+        res.sendFile(path.join(__dirname + '/views/contact_us.html.html'));
+})
+
+//---------------------------------------------------------------
+app.use(function(err, req,res,next){
+    console.error(err.stack)
+    res.status(500).send("Something broke :/");
+})
+
+//if neither a page router or error were triggered then we 
+//likely have a 404 error, send this file then
+app.use(function(req, res, next){
+    res.status(404);
+    res.sendfile(path.join(__dirname+"/views/status404.html"))
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
